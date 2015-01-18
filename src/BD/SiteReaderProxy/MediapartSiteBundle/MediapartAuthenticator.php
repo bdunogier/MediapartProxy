17eabab1b520d7b308f2e588f06ede5a610bd189
Bertrand Dunogier <bd@ez.no>
2015-01-18 22:44:04 +0100

    Refactored authenticator

--- src/BD/SiteReaderProxy/MediapartSiteBundle/MediapartAuthenticator.php
+++ src/BD/SiteReaderProxy/MediapartSiteBundle/MediapartAuthenticator.php
@@ -7,9 +7,12 @@
  */
 namespace BD\SiteReaderProxy\MediapartSiteBundle;
 
+use BD\SiteReaderProxyBundle\Proxy\AbstractFormBasedAuthenticator;
 use BD\SiteReaderProxyBundle\Proxy\WebsiteAuthenticator;
 
-class MediapartAuthenticator implements WebsiteAuthenticator, WebsiteAuthenticator\UrlBased, WebsiteAuthenticator\CredentialsBased
+class MediapartAuthenticator
+    extends AbstractFormBasedAuthenticator
+    implements WebsiteAuthenticator, WebsiteAuthenticator\UrlBased, WebsiteAuthenticator\CredentialsBased
 {
     /** @var string */
     private $username;
@@ -19,54 +22,6 @@ class MediapartAuthenticator implements WebsiteAuthenticator, WebsiteAuthenticat
 
     /** @var string */
     private $uri;
-
-    public function login()
-    {
-        $context = stream_context_create(
-            array(
-                'http' => array(
-                    'method' => 'POST',
-                    'header' => 'Content-type: application/x-www-form-urlencoded\r\n',
-                    'content' => http_build_query(
-                        array(
-                            "name" => $this->getUsername(),
-                            "pass" => $this->getPassword(),
-                            "op" => "ok",
-                            "form_id" => "user_login_block"
-                        )
-                    )
-                )
-            )
-        );
-
-        file_get_contents( $this->getUri(), false, $context );
-        foreach ($http_response_header as $headerString) {
-            if (!strstr( ':', $headerString )) {
-                continue;
-            }
-
-            list( $headerName, $headerValue ) = explode( ':', $headerString );
-
-            if ($headerName !== 'Set-Cookie') {
-                continue;
-            }
-
-            if (substr( $headerName, 0, 4 ) == 'SESS') {
-                $sessionCookieString = $headerValue;
-                continue;
-            }
-
-            if (substr( $headerName, 0, 19 ) == 'roles=authenticated') {
-                $roleCookieString = $headerValue;
-            }
-        }
-
-        if (!isset( $sessionCookieString ) || !isset( $roleCookieString )) {
-            throw new \Exception( "No role cookie in loginresponse" );
-        }
-
-        return $sessionCookieString;
-    }
 
     public function setCredentials( $username, $password )
     {
---
+++ src/BD/SiteReaderProxyBundle/Proxy/AbstractFormBasedAuthenticator.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This file is part of the eZ Publish Kernel package
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace BD\SiteReaderProxyBundle\Proxy;
+
+use BD\SiteReaderProxyBundle\Proxy\WebsiteAuthenticator\CredentialsBased;
+use BD\SiteReaderProxyBundle\Proxy\WebsiteAuthenticator\UrlBased;
+
+abstract class AbstractFormBasedAuthenticator implements CredentialsBased, UrlBased
+{
+    public function login()
+    {
+        $context = stream_context_create(
+            array(
+                'http' => array(
+                    'method' => 'POST',
+                    'header' => 'Content-type: application/x-www-form-urlencoded\r\n',
+                    'content' => http_build_query(
+                        array(
+                            "name" => $this->getUsername(),
+                            "pass" => $this->getPassword(),
+                            "op" => "ok",
+                            "form_id" => "user_login_block"
+                        )
+                    )
+                )
+            )
+        );
+
+        file_get_contents( $this->getUri(), false, $context );
+        foreach ($http_response_header as $headerString) {
+            if (!strstr( ':', $headerString )) {
+                continue;
+            }
+
+            list( $headerName, $headerValue ) = explode( ':', $headerString );
+
+            if ($headerName !== 'Set-Cookie') {
+                continue;
+            }
+
+            if (substr( $headerName, 0, 4 ) == 'SESS') {
+                $sessionCookieString = $headerValue;
+                continue;
+            }
+
+            if (substr( $headerName, 0, 19 ) == 'roles=authenticated') {
+                $roleCookieString = $headerValue;
+            }
+        }
+
+        if (!isset( $sessionCookieString ) || !isset( $roleCookieString )) {
+            throw new \Exception( "No role cookie in loginresponse" );
+        }
+
+        return $sessionCookieString;
+    }
+}
